f35246f375a340b7be5743d63467ff973202113d
Tammy <tammycwong@gmail.com>
2021-03-03 20:39:27 -0500

    Finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove()
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id='victory';
+
+document.getElementById('victory');
+
+newHeader.innerHTML="<h1>TAMMY is the champion</h1>";
